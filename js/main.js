5a1dae4a55b5dfa5783496ad4b2af1ed4cc5abf7
sssin1236 <suj0358@gmail.com>
2021-09-24 23:55:56 +0900

    메인 아티클 추가

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 const btnCall = document.querySelector(".btnMo");
-
 const menuMo = document.querySelector(".Mob");
+const frame = document.querySelector("section");
+const list = frame.querySelectorAll("article");
+const deg = 45;
+
+let i = 0;
 
 btnCall.onclick = function(e){
     e.preventDefault();
 
     btnCall.classList.toggle("on");
     menuMo.classList.toggle("on");
+}
+
+for(let el of list){
+    el.style.transform = `rotate(${deg * i}deg)`;
 }
