fa854658986d225e0f5c6217ddd38ea09020f044
sssin1236 <suj0358@gmail.com>
2021-09-26 22:52:12 +0900

    변수설정

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@ const btnCall = document.querySelector(".btnMo");
 const menuMo = document.querySelector(".Mob");
 const frame = document.querySelector("section");
 const lists = frame.querySelectorAll("article");
+const prev = document.querySelector(".btnPrev");
+const next = document.querySelector(".btnNext");
 const deg = -45;
+const len = lists.length-1;
 
 let i = 0;
+let num = 0;
+let active = 0;
 
 
 btnCall.onclick = function(e){
