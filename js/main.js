21f689320d47c8992457456c02f73578c497535f
sssin1236 <suj0358@gmail.com>
2021-09-28 01:25:28 +0900

    이너 수정

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const frame = document.querySelector("section");
 const lists = frame.querySelectorAll("article");
 const prev = document.querySelector(".btnPrev");
 const next = document.querySelector(".btnNext");
-const text = document.querySelector(".inner #txt");
+const text = document.querySelector(".wrap .txt");
 const deg = -45;
 const len = lists.length-1;
 
@@ -60,7 +60,7 @@ function activation(index, lists){
 
 
 const $article = $("article");
-const $txt = $(".txt");
+const $txt = $(".txt2");
 const $top = $(".line1");
 const $right = $(".line2");
 const $bottom = $(".line3");
